e2f249233c282ada245facab1156b34113de3885
arjrao <arjun.rao1@blackrock.com>
2016-11-28 18:41:39 -0500

    Cleaning up

--- bot/fb-bot-functions.js
+++ bot/fb-bot-functions.js
@@ -99,10 +99,10 @@ function processWithWit(sender, message) {
 }
 
 // generic function sending messages
-function sendToMessenger( recipientId, message ) {
+function sendToMessenger( id, text ) {
   const body = JSON.stringify({
     recipient: { recipientId },
-    message: { message },
+    message: { text },
   });
   const qs = 'access_token=' + encodeURIComponent(Config.FB_PAGE_ACCESS_TOKEN);
   return fetch('https://graph.facebook.com/me/messages?' + qs, {
