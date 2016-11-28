0dc5d2f63678dbd53cb190102cdecafd9f069f66
arjrao <arjun.rao1@blackrock.com>
2016-11-28 17:15:38 -0500

    Updating send action

--- bot/wit.js
+++ bot/wit.js
@@ -31,9 +31,29 @@ var firstEntityValue = function (entities, entity) {
 
 const actions = {
 
-	send (request,response) {
-		console.log('sending...', JSON.stringify(response));
-		return Promise.resolve();
+	send ({sessionId}, {text}) {
+		// Our bot has something to say!
+		// Let's retrieve the Facebook user whose session belongs to
+		const recipientId = sessions[sessionId].fbid;
+		if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
 	},
 
 	merge({entities, context, message, sessionId,cb})  {
