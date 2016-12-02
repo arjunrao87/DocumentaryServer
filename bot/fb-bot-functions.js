745ee3ec7c820dab036e9854ef97a495c3296600
arjrao <arjun.rao1@blackrock.com>
2016-12-02 17:14:11 -0500

    Generic message

--- bot/fb-bot-functions.js
+++ bot/fb-bot-functions.js
@@ -1,9 +1,8 @@
 var request = require('request');
 var Config = require('../config');
-//var Wit = require('node-wit').Wit;
 const {Wit, log} = require('node-wit');
 var sessions = {};
-
+const wit = getWit();
 
 module.exports={
   getHook,
@@ -72,19 +71,11 @@ function processWithWit(sender, message) {
 		var sessionId = findOrCreateSession(sender);
     console.log( "processWithWit :: Sender = " + sender + ", sessionId = " + sessionId + ", text = " + message + ", context = " + JSON.stringify(sessions[sessionId].context) );
 		wit.runActions(
-			sessionId, // the user's current session by id
-			message,  // the user's message
+			sessionId,
+			message,
 			sessions[sessionId].context)
     .then((context) => {
   				console.log('Waiting for further messages');
-  				// Based on the session state, you might want to reset the session
-  				// Example:
-  				// if (context['done']) {
-  				// 	delete sessions[sessionId]
-  				// }
-  				// Updating the user's current session state
-  				// sessions[sessionId].context = context
-          console.log( "In the RunActions Then, context = " + JSON.stringify( context));
   			})
     .catch((err) => {
         console.error('Oops! Got an error from Wit: ', err.stack || err);
@@ -92,7 +83,6 @@ function processWithWit(sender, message) {
   }
 }
 
-// generic function sending messages
 function sendToMessenger( id, text ) {
   const body = JSON.stringify({
     recipient: { id },
@@ -113,74 +103,17 @@ function sendToMessenger( id, text ) {
   });
 };
 
-
-//-------------------------
-
-
 const actions = {
 
-  // send (request,response) {
   send({sessionId}, {text}){
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    // console.log( "RESPONSE TEXT object = " + JSON.stringify( response.text ) );
-    //const recipientId = request.context._fbid_;
-    // console.log( "The id maybe " + sessionId);
-    // console.log( "Sessions object - " + JSON.stringify( sessions ) );
-    // const recipientId = sessions[sessionId].fbid;
-    // console.log( "recipientId = " + recipientId);
-    // if (recipientId) {
-    //   return sendToMessenger(recipientId, text)
-    //   .then(() => null)
-    //   .catch((err) => {
-    //     console.error(
-    //       'Oops! An error occurred while forwarding the response to',
-    //       recipientId,
-    //       ':',
-    //       err.stack || err
-    //     );
-    //   });
-    // }
-    // else {
-    //   console.error('Oops! Couldn\'t find user for session:', sessionId);
-    //   return Promise.resolve()
-    // }
-
     return new Promise(function(resolve, reject) {
-            console.log(JSON.stringify(text));
-            const recipientId = sessions[sessionId].fbid;
-            console.log( "recipientId = " + recipientId);
-            sendToMessenger(recipientId, text)
-            return resolve();
+      const recipientId = sessions[sessionId].fbid;
+      sendToMessenger(recipientId, text)
+      return resolve();
     });
   },
 
   merge({entities, context, message, sessionId,cb})  {
-    // // Reset the recommendation story
-    // delete context.getRecommendations;
-    //
-    // // Retrive the location entity and store it in the context field
-    // var duration = firstEntityValue(entities, 'duration');
-    // if (duration) {
-    //  context.duration = duration;
-    // }
-    //
-    // // Retrieve the category
-    // var searchQuery = firstEntityValue(entities, 'searchQuery')
-    // if (searchQuery) {
-    //  context.searchQuery = searchQuery;
-    // }
-    //
-    // // Retrieve the sentiment
-    // var sentiment = firstEntityValue(entities, 'sentiment')
-    // if (sentiment) {
-    //  context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
-    // } else {
-    //  delete context.ack
-    // }
-    // cb(context);
-    console.log( "In the MERGE method ");
-
     return new Promise(function(resolve, reject) {
       return resolve(context);
     });
@@ -189,73 +122,12 @@ const actions = {
   error(request) {
     console.log(JSON.stringify( request ))
   },
+
   getRecommendations({sessionId, context, text, entities}) {
-        console.log(`Session ${sessionId} received ${text}`);
-        console.log(`The current context is ${JSON.stringify(context)}`);
-        console.log(`Wit extracted ${JSON.stringify(entities)}`);
         return Promise.resolve(context);
   }
-
-  // // list of functions Wit.ai can execute
-  // ['fetch-weather'](sessionId, context, cb) {
-  //  // Here we can place an API call to a weather service
-  //  // if (context.loc) {
-  //  //  getWeather(context.loc)
-  //  //    .then(function (forecast) {
-  //  //      context.forecast = forecast || 'sunny'
-  //  //    })
-  //  //    .catch(function (err) {
-  //  //      console.log(err)
-  //  //    })
-  //  // }
-  //
-  //  context.forecast = 'Sunny'
-  //
-  //  cb(context)
-  // },
 }
 
-// // BOT TESTING MODE
-// if (require.main === module) {
-//  console.log('Bot testing mode!')
-//  var client = getWit();
-//  client.interactive();
-// }
-
-// GET WEATHER FROM API
-// var getWeather = function (location) {
-//  return new Promise(function (resolve, reject) {
-//    var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ location +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-//    request(url, function (error, response, body) {
-//        if (!error && response.statusCode == 200) {
-//          var jsonData = JSON.parse(body)
-//          var forecast = jsonData.query.results.channel.item.forecast[0].text
-//          console.log('WEATHER API SAYS....', jsonData.query.results.channel.item.forecast[0].text)
-//          return forecast
-//        }
-//      })
-//  })
-// }
-
-// CHECK IF URL IS AN IMAGE FILE
-var checkURLIsImage = function (url) {
-    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
-}
-
-var firstEntityValue = function (entities, entity) {
-  var val = entities && entities[entity] &&
-    Array.isArray(entities[entity]) &&
-    entities[entity].length > 0 &&
-    entities[entity][0].value
-
-  if (!val) {
-    return null
-  }
-  return typeof val === 'object' ? val.value : val
-}
-const wit = getWit();
-
-// SETUP THE WIT.AI SERVICE
 function getWit() {
   return new Wit({
   accessToken: Config.WIT_TOKEN,
