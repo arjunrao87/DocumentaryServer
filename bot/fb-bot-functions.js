a89e5824624926d00ccaeedd69b0d75cd8390434
arjrao <arjun.rao1@blackrock.com>
2016-12-02 15:43:03 -0500

    Switching ids

--- bot/fb-bot-functions.js
+++ bot/fb-bot-functions.js
@@ -96,7 +96,7 @@ function processWithWit(sender, message) {
 // generic function sending messages
 function sendToMessenger( id, text ) {
   const body = JSON.stringify({
-    recipient: { "1480521671962588" },
+    recipient: "1480521671962588",
     message: { text },
   });
   const qs = 'access_token=' + encodeURIComponent(Config.FB_PAGE_ACCESS_TOKEN);
