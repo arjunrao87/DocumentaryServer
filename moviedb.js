5ddf30ee90b6abecbbca736ab923f27fccea45d4
Arjun <rao.arjun87@gmail.com>
2017-09-16 18:00:44 -0400

    Adding movie details api

--- moviedb.js
+++ moviedb.js
@@ -6,6 +6,7 @@ var rp = require('request-promise');
 const BASE_URL = "https://api.themoviedb.org/3/"
 const API_KEY = "3441b7624a92e83675247a43dad1ee91";
 const SEARCH_URL = BASE_URL + "search/movie?api_key=" + API_KEY + "&query="
+const DETAILS_URL = "?api_key="+ API_KEY + "&append_to_response=videos";
 
 async function searchQuery( query ){
   const promise = searchForString( query );
@@ -15,6 +16,26 @@ async function searchQuery( query ){
   } catch ( error ){
     console.log( "Error = " + error )
   }
+}
+
+async function getMovieDetailsForId( id ){
+  const promise = retrieveMovieDetailsForId( id );
+  try{
+    let results = await Promise.resolve( promise );
+    return results;
+  } catch ( error ){
+    console.log( "Error = " + error )
+  }
+}
+
+function retrieveMovieDetailsForId( id ){
+  const url = BASE_URL + `movie/${id}` + DETAILS_URL;
+  var options = {
+    method: 'GET',
+    uri: url,
+    json: true
+  };
+  return rp( options );
 }
 
 function searchForString( searchQuery ){
@@ -29,7 +50,8 @@ function searchForString( searchQuery ){
 
 module.exports={
   searchForString,
-  searchQuery
+  searchQuery,
+  getMovieDetailsForId
 };
 
 // Additional queries :
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const resolvers = {
     async search (root, {input}) {
       const results = await MOVIE_DB.searchQuery( input.query );
       return results.results; // This is an array of maps
+    },
+    async getMovieDetailsForId(root, {id}){
+      const results = await MOVIE_DB.getMovieDetailsForId( id );
+      return results; // This is a map
     }
   },
 
@@ -35,6 +39,12 @@ const resolvers = {
       return "bar"
     },
   },
+
+  MovieDetails : {
+    videos( root, args ){
+      return root.videos.results
+    }
+  }
 };
 
 const typeDefs =
@@ -66,6 +76,73 @@ const typeDefs =
     release_date : String
   }
 
+  type MovieDetails{
+    id : ID!
+    adult : Boolean
+    backdrop_path : String
+    belongs_to_collection : CollectionName
+    budget : Float
+    genres : [Genre]
+    homepage : String
+    imdb_id : String
+    original_language : String
+    original_title : String
+    overview : String
+    popularity : Float
+    poster_path : String
+    production_companies : [ProductionCompany]
+    production_countries : [ProductionCountry]
+    release_date : String
+    revenue : Float
+    runtime : Int
+    spoken_languages : [SpokenLanguage]
+    status : String
+    tagline : String
+    title : String
+    vote_count : Int
+    video : Boolean
+    vote_average : Float
+    videos : [VideoBlurb]
+  }
+
+  type VideoBlurb{
+    id : String
+    iso_639_1 : String
+    iso_3166_1 : String
+    key : String
+    name : String
+    site : String
+    size : Int
+    type : String
+  }
+
+  type SpokenLanguage{
+    iso_639_1 : String
+    name : String
+  }
+
+  type ProductionCompany{
+    name : String
+    id : Int
+  }
+
+  type ProductionCountry{
+      iso_3166_1 : String
+      name : String
+  }
+
+  type CollectionName{
+    id : Int
+    name : String
+    poster_path : String
+    backdrop_path : String
+  }
+
+  type Genre{
+    id : Int
+    name : String
+  }
+
   type PageInfo {
     hasNextPage: Boolean
     hasPreviousPage: Boolean
@@ -81,6 +158,7 @@ const typeDefs =
 
   type Query{
     search( input:SearchQuery! ) : MovieConnection
+    getMovieDetailsForId( id : String! ) : MovieDetails
   }
 `
 ;
